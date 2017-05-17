5d6b0af685b61d373c4679f7d1275b5c4255f39a
Attila Gergely <attila@gergely.co>
2017-05-17 14:25:38 +0200

    FIX chisight's mentioned check off stringComplete on serialEvent

--- rgb_arduino.ino
+++ rgb_arduino.ino
@@ -64,15 +64,17 @@ void loop() {
 
 
 void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag
-    // so the main loop can do something about it:
-    if (inChar == '\n') {
-      stringComplete = true;
+  if(!stringComplete) {
+    while (Serial.available()) {
+      // get the new byte:
+      char inChar = (char)Serial.read();
+      // add it to the inputString:
+      inputString += inChar;
+      // if the incoming character is a newline, set a flag
+      // so the main loop can do something about it:
+      if (inChar == '\n') {
+        stringComplete = true;
+      }
     }
   }
 }
